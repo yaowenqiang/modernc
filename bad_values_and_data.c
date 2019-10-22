3f09564b5626c07e6e9dceb3bcbd0b7dabaaae09
yaowenqiang <yaowenqiang111@163.com>
2019-10-22 23:31:01 +0800

    add bad values demo and test vscode debug function

---
+++ bad_values_and_data.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <float.h>
+
+int main()
+{
+    double a = 1;
+    signed short b = -1;
+    signed int c = 0x800000000;
+    signed int d = -0x800000000;
+    signed int e = -2147483648;
+    unsigned short g = 0x800000000;
+
+    return 0;
+
+
+}
